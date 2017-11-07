04443e19305983d9462f07aef3c1e5069e249229
Jacob Johnsson <jacob.johnsson1992@gmail.com>
2017-11-07 15:04:40 +0100

    Add files via upload

--- src/main/scala/turtlerace/TurtleRace.scala
+++ src/main/scala/turtlerace/TurtleRace.scala
@@ -9,8 +9,35 @@ class TurtleRace(val millisPerStep: Int = 5) {
     rw: RaceWindow,
     title: String
   ): Seq[RaceTurtle] = {
+
+    /*  1. Sätt alla turtle på respektive startposition
+        2. while(någon inte vunnit)
+           Alla som INTE vinnut tar ett steg
+           Om (någon gick i mål
+              flytta de nturtlen till en Seq[] som representerar vinnarna i korrekt ordning
+     */
+    val winners = Array.fill(8)[RaceTurtle]
+    val winnerIndex = 0
+
+    var i = 0
+    for (t <- turtles.indices)
+      turtles(t).jumpTo(graphics.Point(rw.startY(i), rw.startY(i)))
+
+    while (true) {
+      var t = 0
+      for (t <- turtles.indices) {
+
+        turtles(t).forward(1.0)
+        if (turtles(t).x > rw.endX)
+          winners(winnerIndex) = turtles(t)
+        i += 1
+      }
+    }
+
+
     rw.writeTitle("TurtleRace NOT READY")
     rw.draw()
     Seq()
   }
+
 }
