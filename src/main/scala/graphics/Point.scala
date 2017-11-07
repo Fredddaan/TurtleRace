998ec838c45a6995e0090251b3345d498ba49e8c
Jacob Johnsson <jacob.johnsson1992@gmail.com>
2017-11-07 13:44:06 +0100

    Uppdaterade Point.scala

--- src/main/scala/graphics/Point.scala
+++ src/main/scala/graphics/Point.scala
@@ -1,3 +1,13 @@
 package graphics
 
-case class Point(x: Double, y: Double) 
+case class Point(val x: Double = 0, val y: Double = 0) {
+  val r = math.hypot(x, y);
+  val theta = math.atan2(x, y);
+  def negY: Point = new Point(x, -y)
+  def +(p: Point) = new Point(x + p.x, y + p.y)
+  override def toString = s"$x, $y";
+}
+object Point {
+  def polar(r: Double, theta: Double) = new Point(
+    math.cos(theta) * r, math.sin(theta) * r )
+}
