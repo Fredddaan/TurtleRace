059645838704e69e5a27b0d7422083664b50555b
Jacob Johnsson <jacob.johnsson1992@gmail.com>
2017-11-08 20:20:36 +0100

    Add files via upload

--- src/main/scala/turtlerace/TurtleRace.scala
+++ src/main/scala/turtlerace/TurtleRace.scala
@@ -5,28 +5,30 @@ package turtlerace
 import graphics._
 import cslib.window._
 
-class TurtleRace(val millisPerStep: Int = 5) {
+class TurtleRace(val millisPerStep: Int = 10) {
   def race(
     turtles: Seq[RaceTurtle],
     rw: RaceWindow,
     title: String
   ): Seq[RaceTurtle] = {
+    rw.draw()
 
     // Skapa en lista fom representerar vinnarna i korrekt ordning 0 - n.
     val winners = new Array[RaceTurtle](turtles.length)
 
     // 1. Sätt alla turtles på respektive startposition
-    for (t <- turtles.indices)
-      turtles(t) jumpTo Point(rw.startY(t), rw.startY(t))
+    for (t <- turtles.indices) {
+      turtles(t) jumpTo Point(rw.startX, rw.startY(t))
+    }
 
     // 2. Sätt en variabel som håller koll på hur många som gått i mål.
-    var iMål = 0;
+    var iMål = 0
 
     // 3. While(iMål < antalet tävlande)
     while (iMål < turtles.length) {
 
       // 3.1 Alla som INTE vunnit tar ett steg
-      for (t <- iMål until turtles.length - 1) {
+      for (t <- iMål until turtles.length) {
         turtles(t).raceStep()
 
         // 3.2 Om (någon gick i mål)
@@ -38,12 +40,11 @@ class TurtleRace(val millisPerStep: Int = 5) {
           // Inkrementera antalet som gått i mål
           iMål += 1
         }
+        Thread.sleep(millisPerStep)
       }
     }
 
     rw.writeTitle("TurtleRace MIGHT BE READY")
-    rw.draw()
-
     // 4. Returnera listan av vinnare
     winners.toSeq
   }
